d1d546113164b51510a2ad61e5eaf6c7b52116ad
Jessica <jchu0398@gmail.com>
2020-11-21 15:52:16 -0500

    Read from file 'test.txt'

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import java.io.*;
+
 public class App {
+
+    public static void doesFileExist(String path) throws Exception {
+
+        // Open file in path
+        File file = new File(path);
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String str;
+            while ((str = br.readLine()) != null) {
+                
+                // Split string at " - " and ", "
+                String[] arrOfStr = str.split(" - |, ");
+
+                for (String a : arrOfStr) {
+                    System.out.println(a);
+                }
+                
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.print("\nFile does not exist.\n\n");
+
+            // Prints out errors
+            e.printStackTrace();
+        }
+        
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        // doesFileExist("src/test.txt");
+        doesFileExist("test.txt");
     }
 }
